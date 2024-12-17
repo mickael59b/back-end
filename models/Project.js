a0466738b3bbf85c810f9329f86668dd076c6ddc
mickael59b <mboutte621@gmail.com>
2024-12-17 12:06:26 +0100

    correction bug route contact

--- models/Project.js
+++ models/Project.js
@@ -2,11 +2,29 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  category: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String },  // Facultatif, si nécessaire
-}, { timestamps: true });
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    default: null,
+  },
+  imageName: {
+    type: String,
+    default: null,
+  },
+}, {
+  timestamps: true,
+});
 
 const Project = mongoose.model('Project', projectSchema);
 
